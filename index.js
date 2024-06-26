5a1f644c814e43766b10b545772c1bc892e552a3
Jackson <jacksonsteely@yahoo.com>
2024-06-26 18:06:34 -0400

    JS-Project

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 // API 1: "http://www.omdbapi.com/?apikey=3a666101&s=fast"
 
-async function main() {
-  const movies = await fetch("http://www.omdbapi.com/?apikey=3a666101&s=fast");
-  const moviesData = await movies.json();
-  console.log(moviesData);
-  // const moviesList = document.querySelector();
+const movieList = document.querySelector(".movie-list");
 
-  // moviesList.innerHTML = moviesData
+async function main() {
+  const movie = await fetch("http://www.omdbapi.com/?apikey=3a666101&s=fast");
+  const movieData = await movie.json();
+  movieList.innerHTML = movieData.map((movie) => movieHTML(movie)).join("");
 }
 
 main();
+
+function movieHTML(movie) {
+  return `<div class="movie-card">
+  <div class="movie-card__container">
+    <div class="movie__img">
+      <img src="${movie.Poster}" />
+    </div>
+    <h3>${movie.Title}</h3>
+    <p>
+      <b>${movie.Year}:</b> 0000
+    </p>
+    <p>
+      <b>${movie.imdbID}:</b>
+    </p>
+  </div>
+</div>`;
+}
